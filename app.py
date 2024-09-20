9fc561bd542edfe42dbaae52c3fe50da7e4bd4be
Aisling Carroll <aislingcarroll0801@gmail.com>
2024-09-20 19:41:37 +0100

    Added route for home page and functionality to collect and store a user name

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, session, request, redirect, url_for
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/set_name', methods=['POST'])
+def set_name():
+    session ['username'] = request.form['name']
+    return redirect(url_for'todo')
+
+if __name__ == '__main__':
+    app.run(debug=True)
