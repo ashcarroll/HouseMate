668e910be67f195bf873c993778a61d81855541a
Aisling Carroll <aislingcarroll0801@gmail.com>
2024-09-20 23:23:09 +0100

    Fix for missing bracket error in set_name function Added in  name attribute to the input to the index html that I had forgotten Started adding to-do list functionality and build out to-do list html page

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, session, request, redirect, url_for
 
 app = Flask(__name__)
 
+tasks = []
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -9,7 +11,15 @@ def home():
 @app.route('/set_name', methods=['POST'])
 def set_name():
     session ['username'] = request.form['name']
-    return redirect(url_for'todo')
+    return redirect(url_for('todo'))
+
+@app.route('/todo', methods=['GET', 'POST'])
+def todo():
+    if request.method == 'POST':
+        task = request.form['task']
+        if task.strip():
+            tasks.append({'task':task, 'added by': session['username']})
+    return render_template('todo.html', tasks=tasks)
 
 if __name__ == '__main__':
     app.run(debug=True)
