1bc36a5d92f3cab519e18d19efe956096347a9e5
Aisling Carroll <aislingcarroll0801@gmail.com>
2024-09-22 18:28:18 +0100

    Added js to remove completed task from list and adds it to server

--- static/script.js
+++ static/script.js
@@ -12,12 +12,34 @@ function formSubmit(event) {
     event.preventDefault();
 
     let form = event.target;
-    removeCompletedTask(form);
+    submitTaskToServer(form);
 }
 
-function removeCompletedTask(form) {
-    let doneTask = form.closest('li');
-    if (doneTask) {
-        doneTask.remove();
+function submitTaskToServer(form) {
+    let fetchOptions = {
+        method: 'POST',
+        body: getTaskInfo(form)
+    };
+
+    fetch(form.action, fetchOptions)
+        .then(function(response) {
+            handleResponse(response, form);
+        })
+}
+
+function getTaskInfo(form) {
+    return new URLSearchParams(new TaskInfo(form));
+}
+
+function handleResponse(response, form) {
+    if (response.status === 204) {
+        removeCompletedTask(form);
+    } 
+        else {
+            console.error('Unexpected status:', response.status);
     }
+}
+
+function removeCompletedTask {
+    form.parentElement.remove();
 }
